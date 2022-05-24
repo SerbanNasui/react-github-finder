d7f86e2a1d21767b74b7676e18dd9c4d97742155
Serban <serban.nasui@gosocialdev.eu>
2022-05-24 17:16:07 +0300

    navbar items

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Navbar = ({icon,title}) => {
-
+const Navbar = ({ icon, title }) => {
     return (
-        <nav className='navbar bg-dark'>
-            <h1>
-                <i className={icon}></i> {title}
-            </h1>
-        </nav>
-    )
-}
+      <nav className='navbar bg-dark'>
+        <h1>
+          <i className={icon} /> {title}
+        </h1>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
 
 Navbar.defaultProps = {
     title: 'Github Finder',
@@ -22,4 +30,4 @@ Navbar.propTypes = {
     icon: PropTypes.string.isRequired
 }
 
-export default Navbar
+export default Navbar;
