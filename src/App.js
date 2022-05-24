9a1561ef9ba2fb5d9ff21c53317b50162e315e72
Serban <serban.nasui@gosocialdev.eu>
2022-05-24 18:01:12 +0300

    user profile

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, {Component, Fragment} from 'react';
+import React, { Fragment, Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import axios from 'axios';
+import './App.css';
 
 import './App.css';
 class App extends Component{
 
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
@@ -32,9 +35,21 @@ class App extends Component{
     setTimeout(()=> this.setState({ alert: null}),2000);
   }
 
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  };
+
   render(){
 
-    const { loading, users } = this.state;
+    const { loading, users, user } = this.state;
 
     return (
       <Router>
@@ -59,6 +74,14 @@ class App extends Component{
                 )}
               />
               <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props =>(
+                <User
+                  {...props}
+                  getUser={this.getUser}
+                  user={user}
+                  loading={loading}
+                />
+              )} />
             </Switch>
           </div>
         </div>
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const UserItem  = ({user:{login, avatar_url, html_url}}) => {
     return (
@@ -12,7 +13,7 @@ const UserItem  = ({user:{login, avatar_url, html_url}}) => {
             />
             <h3>{login}</h3>
             <div>
-                <a href={html_url} className='btn btn-dark btn-sm my-1' target={'_blank'}>More Info</a>
+                <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>More Info</Link>
             </div>
         </div>
     );
