3bc8274e77a734adaa94cb7f216380e54485f54a
Serban <serban.nasui@gosocialdev.eu>
2022-05-24 15:55:57 +0300

    search github profiles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+class App extends Component{
+
+  state = {
+    users: [],
+    loading: false,
+    alert: null
+  }
+  
+  searchUsers = async text =>{
+
+    this.setState({loading: true})
+
+    const response = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    
+    this.setState({users: response.data.items, loading: false});
+  }
+
+  clearUsers = () => this.setState({ users:[], loading: false });
+
+  setAlert = (msg,type) => {
+    this.setState({alert: {msg, type} });
+    setTimeout(()=> this.setState({ alert: null}),2000);
+  }
+
+  render(){
+
+    const { loading, users } = this.state;
+
+    return (
+      <div className='App'>
+        <Navbar/>
+        <div className='container'>
+          <Alert alert={this.state.alert}/>
+          <Search 
+            searchUsers={this.searchUsers} 
+            clearUsers={this.clearUsers}
+            showClear={ users.length > 0 ? true : false }
+            setAlert={this.setAlert}
+          />
+          <Users 
+            loading={loading} 
+            users={users}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
